2817b461497c37c427409c9c43a1d4f0d1d596cb
Shiqi Sheng <shiqisheng00@gmail.com>
2023-03-06 23:16:25 -0800

    added quiz feature

--- japanese.go
+++ japanese.go
@@ -1,5 +1,3 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import (
@@ -11,35 +9,56 @@ import (
 	"time"
 )
 
-// have a hash map of the correct answers
-// have a list of hiragana
-// have a list of letters
-// have a generate random function
-// have a get correct answers function
-// slice should be in format of string slice with each as individual letters
-
 func main() {
-	cmd := exec.Command("clear") //Linux example, its tested
+	// Clear screen.
+	cmd := exec.Command("clear")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
-	//hiragana := []string{"あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ"}
-	row4 := []string{"た", "ち", "つ", "て", "と"}
-	words := row4
+
+	// Builds list of hiragana to practice.
+	hr1 := []string{"あ", "い", "う", "え", "お"}
+	hr2 := []string{"か", "き", "く", "け", "こ"}
+	hr3 := []string{"さ", "し", "す", "せ", "そ"}
+	hr4 := []string{"た", "ち", "つ", "て", "と"}
+	hr5 := []string{"な", "に", "ぬ", "ね", "の"}
+	h := [][]string{hr1, hr2, hr3, hr4, hr5}
+	words := make([]string, 0)
+	for _, v := range h {
+		words = append(words, v...)
+	}
+
+	// Shuffles list.
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(words), func(i, j int) {
 		words[i], words[j] = words[j], words[i]
 	})
 
-	fmt.Println(correctAnswer(words))
-	fmt.Print("insert y value here: ")
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-	fmt.Println(input.Text())
-	fmt.Println(words)
+	// Quiz the user for each word.
+	for _, word := range words {
+		fmt.Println(word)
+		fmt.Print("Type in your answer:")
+		complete := false
+		for !complete {
+			input := bufio.NewScanner(os.Stdin)
+			input.Scan()
+			ans, err := correctAnswer(word)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if input.Text() == ans {
+				fmt.Println("Correct!")
+				complete = true
+			} else {
+				fmt.Println("Incorrect. Try again.")
+			}
+		}
+	}
+
+	fmt.Println("Practice complete.")
 }
 
-func correctAnswer(input []string) []string {
-	m := map[string]string{
+func correctAnswer(input string) (string, error) {
+	hiraganaToLetters := map[string]string{
 		"あ": "a",
 		"い": "i",
 		"う": "u",
@@ -60,11 +79,16 @@ func correctAnswer(input []string) []string {
 		"つ": "tsu",
 		"て": "te",
 		"と": "to",
+		"な": "na",
+		"に": "ni",
+		"ぬ": "nu",
+		"ね": "ne",
+		"の": "no",
 	}
 
-	output := []string{}
-	for _, s := range input {
-		output = append(output, m[s])
+	val, ok := hiraganaToLetters[input]
+	if !ok {
+		return "", fmt.Errorf("Input not found")
 	}
-	return output
+	return val, nil
 }
